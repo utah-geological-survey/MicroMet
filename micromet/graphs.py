c347c3cd2be8569e253edbc14aab2e832470dd68
Paul <paulinkenbrandt@utah.gov>
2025-04-26 16:37:07 -0600

    Refactor: remove unused database_update.py and consolidate configuration loading in station_data_pull.py; enhance graph plotting functions in tools.py

--- micromet/graphs.py
+++ micromet/graphs.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def energy_sankey(df, date_text="2024-06-19 12:00"):
     """
     Create a Sankey diagram representing the energy balance for a specific date and time.
@@ -82,7 +83,7 @@ def energy_sankey(df, date_text="2024-06-19 12:00"):
         "Ground Heat Flux",
         "Sensible Heat",
         "Latent Heat",
-        "Residual"
+        "Residual",
     ]
 
     print(h)
@@ -100,46 +101,136 @@ def energy_sankey(df, date_text="2024-06-19 12:00"):
     values = [lwi, swi, nr, swo, lwo, shf, h, le, rem]  # Values of the energy flow
 
     # Create the Sankey diagram
-    fig = go.Figure(data=[go.Sankey(
-        node=dict(
-            pad=15,
-            thickness=20,
-            line=dict(color="black", width=0.5),
-            label=labels,
-        ),
-        link=dict(
-            source=source,
-            target=target,
-            value=values,
-        )
-    )])
+    fig = go.Figure(
+        data=[
+            go.Sankey(
+                node=dict(
+                    pad=15,
+                    thickness=20,
+                    line=dict(color="black", width=0.5),
+                    label=labels,
+                ),
+                link=dict(
+                    source=source,
+                    target=target,
+                    value=values,
+                ),
+            )
+        ]
+    )
 
     # Update layout and title
-    fig.update_layout(title_text=f"Energy Balance {ebr:0.2f} on {select_date:%Y-%m-%d}", font_size=10)
+    fig.update_layout(
+        title_text=f"Energy Balance {ebr:0.2f} on {select_date:%Y-%m-%d}", font_size=10
+    )
 
     # Show the figure
     # fig.show()
     return fig
 
-def bland_altman_plot(data1, data2, *args, **kwargs):
-    data1     = np.asarray(data1)
-    data2     = np.asarray(data2)
-    mean      = np.mean([data1, data2], axis=0)
-    diff      = data1 - data2                   # Difference between data1 and data2
-    md        = np.mean(diff)                   # Mean of the difference
-    sd        = np.std(diff, axis=0)            # Standard deviation of the difference
-    CI_low    = md - 1.96*sd
-    CI_high   = md + 1.96*sd
 
-    plt.scatter(mean, diff, *args, **kwargs)
-    plt.axhline(md,           color='black', linestyle='-')
-    plt.axhline(md + 1.96*sd, color='gray', linestyle='--')
-    plt.axhline(md - 1.96*sd, color='gray', linestyle='--')
-    return md, sd, mean, CI_low, CI_high
+def scatterplot_instrument_comparison(edmet, compare_dict, station):
+    # Compare two instruments
+    instruments = list(compare_dict.keys())
+    df = edmet[instruments].replace(-9999, np.nan).dropna()
+    df = df.resample("1h").mean().interpolate(method="linear")
+    df = df.dropna()
+
+    x = df[instruments[0]]
+    y = df[instruments[1]]
+
+    xinfo = compare_dict[instruments[0]]
+    yinfo = compare_dict[instruments[1]]
+
+    # one to one line
+    xline = np.arange(df.min().min(), df.max().max(), 0.1)
+    # Perform linear regression
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
+    # Predict y values
+    y_pred = slope * x + intercept
+    # R-squared
+    r_squared = r_value**2
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    # Plot
+    ax.scatter(x, y, alpha=0.5, s=1, label="Data points")
+    ax.set_title(f"{xinfo[1]} Comparison: {station}")
+    ax.plot(xline, xline, label="1:1 line", color="green", linestyle="--")
+    ax.plot(
+        x,
+        y_pred,
+        color="red",
+        label=f"Fit: y = {slope:.2f}x + {intercept:.2f}\n$R^2$ = {r_squared:.3f}",
+    )
+    plt.legend()
+    plt.grid(True)
+
+    ax.set_xlabel(f"{xinfo[0]} {xinfo[1]} ({xinfo[2]})")
+    ax.set_ylabel(f"{yinfo[0]} {yinfo[1]} ({yinfo[2]})")
+
+    plt.show()
+
+    # Print results
+    print(f"Slope: {slope:.3f}")
+    print(f"Intercept: {intercept:.3f}")
+    print(f"R-squared: {r_squared:.3f}")
+    return slope, intercept, r_squared, p_value, std_err, fig, ax
+
+
+def bland_alt_plot(edmet, compare_dict, station, alpha=0.5):
+    # Compare two instruments
+    instruments = list(compare_dict.keys())
+    df = edmet[instruments].replace(-9999, np.nan).dropna()
+    df = df.resample("1h").mean().interpolate(method="linear")
+    df = df.dropna()
+
+    x = df[instruments[0]]
+    y = df[instruments[1]]
+    rmse = np.sqrt(mean_squared_error(x, y))
+    print(f"RMSE: {rmse:.3f}")
+
+    mean_vals = df[instruments].mean(axis=1)
+    diff_vals = x - y
+    bias = diff_vals.mean()
+    spread = diff_vals.std()
+    print(f"Bias = {bias:.3f}, Spread = {spread:.3f}")
+    top = diff_vals.mean() + 1.96 * diff_vals.std()
+    bottom = diff_vals.mean() - 1.96 * diff_vals.std()
+
+    f, ax = plt.subplots(1, figsize=(8, 5), alpha=alpha)
+    sm.graphics.mean_diff_plot(x, y, ax=ax)
+    ax.text(
+        mean_vals.mean(),
+        top,
+        s=compare_dict[instruments[0]][0],
+        verticalalignment="top",
+        fontweight="bold",
+    )
+    ax.text(
+        mean_vals.mean(),
+        bottom,
+        s=compare_dict[instruments[1]][0],
+        verticalalignment="bottom",
+        fontweight="bold",
+    )
+    ax.set_title(
+        f"{compare_dict[instruments[0]][0]} vs {compare_dict[instruments[1]][0]} at {station}"
+    )
+    ax.set_xlabel(
+        f"Mean {compare_dict[instruments[0]][1]} ({compare_dict[instruments[0]][2]})"
+    )
+    ax.set_ylabel(
+        f"Difference ({compare_dict[instruments[0]][2]})\n({compare_dict[instruments[0]][0]} - {compare_dict[instruments[1]][0]})",
+        fontsize=10,
+    )
+
+    return f, ax
 
 
 # Example of filtering by date range
-def plot_timeseries_daterange(input_df, selected_station, selected_field, start_date, end_date):
+def plot_timeseries_daterange(
+    input_df, selected_station, selected_field, start_date, end_date
+):
     """
     Args:
         input_df: The DataFrame containing the time series data.
@@ -157,12 +248,11 @@ def plot_timeseries_daterange(input_df, selected_station, selected_field, start_
     filtered_df = filtered_df.loc[:, selected_field].replace(-9999, np.nan)
 
     # Plot each selected category
-    ax.plot(filtered_df.index, filtered_df,
-            label=selected_station, linewidth=2)
+    ax.plot(filtered_df.index, filtered_df, label=selected_station, linewidth=2)
 
-    plt.title(f'{selected_station} {selected_field}\n{start_date} to {end_date}')
-    plt.xlabel('Date')
-    plt.ylabel('Value')
+    plt.title(f"{selected_station} {selected_field}\n{start_date} to {end_date}")
+    plt.xlabel("Date")
+    plt.ylabel("Value")
     plt.legend()
     plt.grid(True)
     plt.xticks(rotation=45)
@@ -177,4 +267,4 @@ def save_plot(b):
     Saves plot for an interactive notebook button function
     """
     # This line saves the plot as a .png file. Change it to .pdf to save as pdf.
-    fig.savefig('plot.png')
+    fig.savefig("plot.png")
--- micromet/station_data_pull.py
+++ micromet/station_data_pull.py
@@ -10,6 +10,17 @@ import configparser
 import sqlalchemy
 from .converter import Reformatter
 
+import configparser
+
+try:
+    config = configparser.ConfigParser()
+    config.read("../secrets/config.ini")
+    passwrd = config["DEFAULT"]["pw"]
+    ip = config["DEFAULT"]["ip"]
+    login = config["DEFAULT"]["login"]
+except KeyError:
+    print("credentials needed")
+
 
 class StationDataManager:
     """
